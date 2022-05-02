e96e3a456f202363cc85d58fbc2b24643809f232
Purya <purya.B97@gmail.com>
2022-05-02 16:41:08 +0300

    GraphQL Data Fetching

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { Layout } from '../components'
 import '../styles/globals.scss'
 import 'tailwindcss/tailwind.css'
-import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
